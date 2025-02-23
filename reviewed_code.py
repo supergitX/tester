6f2c12ddc44c90243316abd91b93958d43a410d1
Naveen Nidadavolu <115283282+navi004@users.noreply.github.com>
2025-02-24 03:21:05 +0530

    Update reviewed_code.py

--- reviewed_code.py
+++ reviewed_code.py
@@ -5,6 +5,6 @@ def factorial(n):
         return 1  
     else:  
         result = 1  
-        for i in range(2, n + 1):  
+        for i in range(2, n + 1): 
             result *= i  
         return result  
